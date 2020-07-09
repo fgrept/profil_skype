a521d8e148dc2da00ca61705d3561ac0763b7adc
Jutige <caludije@gmail.com>
2020-07-09 11:44:37 +0200

    us007 ajout des entites collaborater et itcorrespondant

---
+++ profil-skype/profil-skype-infrastructure/src/main/java/com/bnpparibas/projetfilrouge/pskype/infrastructure/user/CollaboraterEntity.java
@@ -0,0 +1,70 @@
+package com.bnpparibas.projetfilrouge.pskype.infrastructure.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * 
+ * Entité des collaborateurs
+ * @author Judicaël
+ *
+ */
+@Entity
+public class CollaboraterEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUser;
+
+	private String collaboraterId;
+	private String lastName;
+	private String firstName;
+	private String deskPhoneNumber;
+	private String mobilePhoneNumber;
+	private String mailAdress;
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public Long getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+	public String getCollaboraterId() {
+		return collaboraterId;
+	}
+	public void setCollaboraterId(String collaboraterId) {
+		this.collaboraterId = collaboraterId;
+	}
+	public String getDeskPhoneNumber() {
+		return deskPhoneNumber;
+	}
+	public void setDeskPhoneNumber(String deskPhoneNumber) {
+		this.deskPhoneNumber = deskPhoneNumber;
+	}
+	public String getMobilePhoneNumber() {
+		return mobilePhoneNumber;
+	}
+	public void setMobilePhoneNumber(String mobilePhoneNumber) {
+		this.mobilePhoneNumber = mobilePhoneNumber;
+	}
+	public String getMailAdress() {
+		return mailAdress;
+	}
+	public void setMailAdress(String mailAdress) {
+		this.mailAdress = mailAdress;
+	}
+	
+}
