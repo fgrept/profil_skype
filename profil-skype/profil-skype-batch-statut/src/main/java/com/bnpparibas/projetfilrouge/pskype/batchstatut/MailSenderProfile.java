a49635df201069c39004a36bca3e867849149f9f
Jutige <caludije@gmail.com>
2020-07-21 17:20:53 +0200

    us027 : ajout du complément pour la batch de maj de statuts

---
+++ profil-skype/profil-skype-batch-statut/src/main/java/com/bnpparibas/projetfilrouge/pskype/batchstatut/MailSenderProfile.java
@@ -0,0 +1,34 @@
+package com.bnpparibas.projetfilrouge.pskype.batchstatut;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+/**
+ * Classe dédiée à l'envoi de mail via Spring Mail
+ * Elle pourra être externalisée dans la couche infrastructure si le besoin n'est pas circonscrit au batch
+ * @author Judicael
+ *
+ */
+@Component
+public class MailSenderProfile {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	/**
+	 * Envoi d'un mail à partir d'un text de  corps de message et d'un destinataire
+	 * @param message
+	 * @param recipient
+	 * @throws Exception
+	 */
+	public void sendMail(String message, String recipient) throws Exception{
+		
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setSubject("Liste des profils skype désactivés");
+		mail.setText(message);
+		mail.setTo(recipient);
+		javaMailSender.send(mail);
+	}
+}
