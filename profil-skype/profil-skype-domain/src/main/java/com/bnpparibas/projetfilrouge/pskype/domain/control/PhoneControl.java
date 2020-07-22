c1de8b8255b777a636a4715155ef696dd72afbc2
fabien grept <fgrept@gmail.com>
2020-07-22 14:37:44 +0200

    us004 ajout contrôle sur bean et email

--- profil-skype/profil-skype-domain/src/main/java/com/bnpparibas/projetfilrouge/pskype/domain/control/PhoneControl.java
+++ profil-skype/profil-skype-domain/src/main/java/com/bnpparibas/projetfilrouge/pskype/domain/control/PhoneControl.java
@@ -6,10 +6,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Target({ElementType.FIELD,ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PhoneControlValidator.class)
 public @interface PhoneControl {
+ /*   String message() default "Numéro de téléphone non valide selon la norme RFC";
 
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};*/
 }
