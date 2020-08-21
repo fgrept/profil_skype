d9879934a26dcfea6d1e3146510d08237279efb5
Jutige <caludije@gmail.com>
2020-08-21 18:02:17 +0200

    US048 : désactivation de la sécurité

--- profil-skype/profil-skype-exposition/src/main/java/com/bnpparibas/projetfilrouge/pskype/security/SecurityConfiguration.java
+++ profil-skype/profil-skype-exposition/src/main/java/com/bnpparibas/projetfilrouge/pskype/security/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
@@ -30,8 +31,13 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
  * @author Judicaël
  *
  */
+/*
+ * Pour désactiver la sécurité, il faut :
+ * 1) Désactiver la sécurité globale.
+ * 2) Changer la méthode configure pour authoriser toutes les requêtes. 
+ */
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
+//1) @EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
@@ -45,6 +51,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
 		auth.userDetailsService(itCorrespondantUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 	
@@ -58,16 +65,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	 * Méthode d'authorisation d'accès
 	 * Les rôles sont gérés de façon plus fine via l'annotation @Secured
 	 */
+/*2)
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
 		
 		http.csrf().disable()
 		.authorizeRequests()
-//		.formLogin()
-//		.loginProcessingUrl("/login").successHandler(new AuthentificationLoginSuccessHandler())
-//		.and()
-//		.authorizeRequests().antMatchers("/login","/logout").permitAll()
-//		.antMatchers("/profile/**","/cil/**").authenticated().anyRequest().permitAll();
 		.antMatchers("/*").permitAll()
 		.antMatchers("/profile/**","/users/**", "/login","/logout","/collaborater/**","/events/**").permitAll()
 		.anyRequest().authenticated()
@@ -82,11 +85,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.tokenRepository(persistentTokenRepository())
 		.tokenValiditySeconds(TOKEN_VALIDITY_SECONDS)
 		;
-//		.and()
-//		.httpBasic();
-//		http.authorizeRequests().and().exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint());	
 	}
-
+*/
+	/**
+	 * 3) On autorise toutes les requêtes
+	 */
+	@Override
+    public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll();
+	}
+	
 	private PersistentTokenRepository persistentTokenRepository() {
 		
 		final JdbcTokenRepositoryImpl tokenRepositoryImpl = new JdbcTokenRepositoryImpl();
