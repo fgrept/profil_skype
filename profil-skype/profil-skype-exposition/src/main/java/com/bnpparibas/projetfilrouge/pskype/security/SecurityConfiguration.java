49467bc37af256cf2f05ee245227a4e526188e03
Jutige <caludije@gmail.com>
2020-08-24 11:07:57 +0200

    US048 : désactivation csrf

--- profil-skype/profil-skype-exposition/src/main/java/com/bnpparibas/projetfilrouge/pskype/security/SecurityConfiguration.java
+++ profil-skype/profil-skype-exposition/src/main/java/com/bnpparibas/projetfilrouge/pskype/security/SecurityConfiguration.java
@@ -92,7 +92,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
     public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll();
+		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll();
 	}
 	
 	private PersistentTokenRepository persistentTokenRepository() {
